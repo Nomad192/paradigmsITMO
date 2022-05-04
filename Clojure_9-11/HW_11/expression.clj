8f54b97c557d7dee596271184e9efe193375aeb7
Nomad <verenya08@gmail.com>
2022-05-04 16:16:48 +0300

    Add full solution hw11 hard all mod

--- Clojure_9-11/HW_11/expression.clj
+++ Clojure_9-11/HW_11/expression.clj
@@ -36,6 +36,15 @@
 (declare Sinh 	  )
 (declare Cosh	  )
 
+(declare Exp 	  )
+(declare Ln 	  )
+(declare Pow 	  )
+(declare Log 	  )
+(declare Mean 	  )
+(declare Varn	  )
+(declare Sumexp	  )
+(declare Softmax  )
+
 (defn Constant [c] 
 	{
 		:diff 	  (fn [this t] (Constant 0))
@@ -62,6 +71,27 @@
 (defn my-sinh [x] (Math/sinh x))
 (defn my-cosh [x] (Math/cosh x))
 
+(defn my-exp [x] (Math/exp x))
+(defn my-ln  [x] (Math/log (Math/abs x)))
+
+(defn my-log [b x] (/ (Math/log (Math/abs x)) (Math/log (Math/abs b))))
+(defn my-pow [x y] (Math/pow x y))
+
+(defn my-mean [& items] 
+	(double (apply + (mapv (partial * (/ 1 (count items))) items))))
+
+(defn my-varn [& items]
+	(- 
+		(apply + (mapv (partial * (/ 1 (count items))) 
+					   (mapv * items items))) 
+		(Math/pow (apply my-mean items) 2)))
+
+(defn my-sumexp [& items] 
+ 	(apply + (mapv (partial #(Math/exp %)) items)))
+
+(defn my-softmax [& items] 
+    (/ (Math/exp (nth items 0)) (apply my-sumexp items)))
+
 (def op-str {+ 	 '+
 			 - 	 '-
 			 * 	 '*
@@ -76,6 +106,15 @@
 			 my-cos 'cos
 			 my-sinh 'sinh
 			 my-cosh 'cosh
+
+			 my-exp  'exp
+			 my-ln   'ln
+			 my-pow	 'pow
+			 my-log  'log
+			 my-mean 'mean
+			 my-varn 'varn
+			 my-sumexp 	'sumexp
+			 my-softmax	'softmax
 			})
 
 (defn diff-mult-bin [t u v] (Add (Multiply (diff u t) v) (Multiply u (diff v t))))
@@ -107,7 +146,19 @@
 			 'cos 		 Cos
 			 'sinh 		 Sinh
 			 'cosh 		 Cosh
+
+			 'ln    	 Ln
+			 'exp  		 Exp
+			 'pow 		 Pow
+			 'log 		 Log
+			 'mean 		 Mean
+			 'varn   	 Varn
+			 'sumexp  	 Sumexp
+			 'softmax 	 Softmax
          	})
+
+(defn diff-mean [t items]
+ (Divide (apply Add (mapv diff items (iterate identity t))) (Constant (count items))) )
 
 (def ExprPrototype 
 	{
@@ -130,6 +181,15 @@
 				(= op a-m) (Divide (apply Add (mapv diff args (iterate identity t))) (Constant (count args)))
 				(= op g-m) (Divide (diff-mult t args) (Multiply (Constant (count args)) (apply GeomMean args)))
 				(= op h-m) (Divide (apply Add (mapv diff args (iterate identity t))) (Constant (count args)))
+				
+				(= op my-exp) (Multiply (apply Exp args) (diff (first args) t))
+				(= op my-ln) (Divide (diff (first args) t) (first args))
+				(= op my-pow) (Multiply (apply Pow args) (diff (Multiply (second args) (Ln (first args))) t))
+				(= op my-log) (diff (Divide (Ln (second args)) (Ln (first args))) t);(Divide (diff (second args) t) (Multiply (second args) (Ln (first args))))
+				(= op my-mean) (diff-mean t args)
+				(= op my-varn) (diff-mean t (mapv (fn [x] (Multiply (Subtract x (apply Mean args)) (Subtract x (apply Mean args)))) args))
+				(= op my-sumexp) (diff (apply Add (mapv Exp args)) t)
+				(= op my-softmax) (diff (Divide (Exp (first args)) (apply Sumexp args)) t)
 				))))
 		:toString (fn [this] 
 						(str 
@@ -168,6 +228,17 @@
 (def Sinh 		(Expr my-sinh))
 (def Cosh		(Expr my-cosh))
 
+
+(def Exp 		(Expr my-exp))
+(def Ln 		(Expr my-ln))
+(def Pow  		(Expr my-pow))
+(def Log 		(Expr my-log))
+(def Mean 		(Expr my-mean))
+(def Varn 		(Expr my-varn))
+(def Sumexp 	(Expr my-sumexp))
+(def Softmax  	(Expr my-softmax))
+
+
 (def str-op {'+ 		 Add
         	 '- 		 Subtract
          	 '* 		 Multiply
@@ -176,12 +247,21 @@
         	 'arith-mean ArithMean
         	 'geom-mean  GeomMean
         	 'harm-mean  HarmMean
-        	 'sum        Sum
-        	 'avg 		 Avg
+			 'sum        Sum
+			 'avg 		 Avg
 			 'sin 		 Sin
 			 'cos 		 Cos
 			 'sinh 		 Sinh
 			 'cosh 		 Cosh
+
+			 'ln    	 Ln
+			 'exp  		 Exp
+			 'pow 		 Pow
+			 'log 		 Log
+			 'mean 		 Mean
+			 'varn   	 Varn
+			 'sumexp  	 Sumexp
+			 'softmax 	 Softmax
          	})
 
 (defn parse [result]
